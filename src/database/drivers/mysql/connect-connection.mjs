b83c0c2f086f64f413ca65c118b9dac20b57b934
kaspernj <k@spernj.org>
2023-07-19 08:29:13 +0000

    Fixed database.mjs issues

--- src/database/drivers/mysql/connect-connection.mjs
+++ src/database/drivers/mysql/connect-connection.mjs
@@ -1,5 +1,5 @@
 // Async function to connect a MySQL connection
-export default async function connectConnection(connection) {
+export default function connectConnection(connection) {
   return new Promise((resolve, reject) => {
     connection.connect((error) => {
       if (error) {
