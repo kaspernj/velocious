a65a77e4647a8b42625876c3499917a29a483642
kaspernj <k@spernj.org>
2023-07-19 11:13:00 +0000

    Removed dead files

--- spec/cli/generate/migration-spec.js
+++
@@ -1,9 +0,0 @@
-import Cli from "../../../src/cli/index.mjs"
-
-describe("Cli - generate - migration", () => {
-  it("generates a new migration", async () => {
-    const cli = new Cli()
-
-    await cli.execute("g", "migration")
-  })
-})
