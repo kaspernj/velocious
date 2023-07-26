4ee0816abfcd89e982fec366f711e13a67f9f417
kaspernj <k@spernj.org>
2023-07-26 17:15:48 +0000

    Fixed CI issues

---
+++ spec/dummy/src/config/configuration.example.mjs
@@ -0,0 +1,20 @@
+import Configuration from "../../../src/configuration.mjs"
+
+const directory = `${process.cwd()}/spec/dummy`
+
+const configuration = new Configuration({
+  database: {
+    default: {
+      master: {
+        type: "mysql",
+        host: "mariadb",
+        username: "username",
+        password: "password",
+        database: "velocious_test"
+      }
+    }
+  },
+  directory
+})
+
+export default configuration
