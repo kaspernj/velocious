d419cb1218c9878ea93b5f9848e6efc67af43c1d
kaspernj <k@spernj.org>
2023-07-28 08:49:28 +0000

    Sort and filter migration files

--- src/cli/commands/db/migrate.mjs
+++ src/cli/commands/db/migrate.mjs
@@ -6,10 +6,20 @@ export default class DbMigrate extends BaseCommand {
   async execute() {
     const projectPath = digg(this.configuration, "directory")
     const migrationsPath = `${projectPath}/src/database/migrations`
+    let files = await fs.readdir(migrationsPath)
 
-    console.log({projectPath, migrationsPath})
+    files = files
+      .map((file) => {
+        const match = file.match(/^(\d{14})-/)
 
-    const files = await fs.readdir(migrationsPath)
+        if (!match) return null
+
+        const date = parseInt(match[1])
+
+        return {file, date}
+      })
+      .filter((file) => Boolean(file))
+      .sort((file1, file2) => file1.date - file2.date)
 
     console.debug({files, migrationsPath})
     console.warn("stub")
