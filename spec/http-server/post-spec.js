b06ec2ff50ec4ec0a7086c8f44897ec3c213b3f1
kaspernj <kasper@diestoeckels.de>
2025-09-07 14:54:21 +0000

    Run request specs multiple times to re-produce the error

--- spec/http-server/post-spec.js
+++ spec/http-server/post-spec.js
@@ -7,66 +7,72 @@ import Project from "../dummy/src/models/project.js"
 describe("HttpServer", () => {
   it("handles post requests", async () => {
     await Dummy.run(async () => {
-      const postData = querystring.stringify({"project[name]": "Test create project"})
-      const response = await fetch(
-        "http://localhost:3006/projects",
-        {
-          body: postData,
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Content-Length": Buffer.byteLength(postData)
-          },
-          method: "POST"
-        }
-      )
-      const text = await response.text()
-      const createdProject = await Project.preload({translations: true}).last()
+      for (let i = 0; i <= 5; i++) {
+        const postData = querystring.stringify({"project[name]": "Test create project"})
+        const response = await fetch(
+          "http://localhost:3006/projects",
+          {
+            body: postData,
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              "Content-Length": Buffer.byteLength(postData)
+            },
+            method: "POST"
+          }
+        )
+        const text = await response.text()
+        const createdProject = await Project.preload({translations: true}).last()
 
-      expect(text).toEqual('{"status":"success"}')
-      expect(createdProject.name()).toEqual("Test create project")
+        expect(text).toEqual('{"status":"success"}')
+        expect(createdProject.name()).toEqual("Test create project")
+      }
     })
   })
 
   it("handles post json requests", async () => {
     await Dummy.run(async () => {
-      const postData = JSON.stringify({project: {name: "Test create project"}})
-      const response = await fetch(
-        "http://localhost:3006/projects",
-        {
-          body: postData,
-          headers: {
-            "Content-Type": "application/json",
-            "Content-Length": Buffer.byteLength(postData)
-          },
-          method: "POST"
-        }
-      )
-      const text = await response.text()
-      const createdProject = await Project.preload({translations: true}).last()
+      for (let i = 0; i <= 5; i++) {
+        const postData = JSON.stringify({project: {name: "Test create project"}})
+        const response = await fetch(
+          "http://localhost:3006/projects",
+          {
+            body: postData,
+            headers: {
+              "Content-Type": "application/json",
+              "Content-Length": Buffer.byteLength(postData)
+            },
+            method: "POST"
+          }
+        )
+        const text = await response.text()
+        const createdProject = await Project.preload({translations: true}).last()
 
-      expect(text).toEqual('{"status":"success"}')
-      expect(createdProject.name()).toEqual("Test create project")
+        expect(text).toEqual('{"status":"success"}')
+        expect(createdProject.name()).toEqual("Test create project")
+      }
     })
   })
 
   it("handles post form-data requests", async () => {
     await Dummy.run(async () => {
-      const body = new FormData()
+      for (let i = 0; i <= 5; i++) {
+        const body = new FormData()
 
-      body.append("project[name]", "Test create project")
+        body.append("project[name]", "Test create project")
 
-      const response = await fetch(
-        "http://localhost:3006/projects",
-        {
-          body,
-          method: "POST"
-        }
-      )
-      const text = await response.text()
-      const createdProject = await Project.preload({translations: true}).last()
+        const response = await fetch(
+          "http://localhost:3006/projects",
+          {
+            body,
+            method: "POST"
+          }
+        )
+        const text = await response.text()
+        const createdProject = await Project.preload({translations: true}).last()
 
-      expect(text).toEqual('{"status":"success"}')
-      expect(createdProject.name()).toEqual("Test create project")
+        expect(text).toEqual('{"status":"success"}')
+        expect(createdProject.name()).toEqual("Test create project")
+      }
     })
   })
 })
