5149af4f87ba329671b944bacfbdc5447dbb3d18
kaspernj <kasper@diestoeckels.de>
2025-09-16 13:41:06 +0000

    Fixed Postgres support as well which was quite annoying

--- spec/database/drivers/columns-spec.js
+++ spec/database/drivers/columns-spec.js
@@ -3,6 +3,14 @@ import dummyConfiguration from "../../dummy/src/config/configuration.js"
 describe("database - drivers - columns", () => {
   it("runs migrations", {databaseCleaning: {transaction: false}}, async () => {
     await dummyConfiguration.ensureConnections(async (dbs) => {
+      // Authentication tokens table is important because it will be totally re-created in SQLite because the renaming of the token column to user-column
+      const authenticationTokensTable = await dbs.default.getTableByName("authentication_tokens")
+      const authenticationTokensIDColumn = await authenticationTokensTable.getColumnByName("id")
+
+      expect(authenticationTokensIDColumn.getAutoIncrement()).toBeTrue()
+      expect(authenticationTokensIDColumn.getPrimaryKey()).toBeTrue()
+      expect(authenticationTokensIDColumn.getNull()).toBeFalse()
+
       const projectsTable = await dbs.default.getTableByName("projects")
       const projectsCreatedAtColumn = await projectsTable.getColumnByName("created_at")
       const projectsIDColumn = await projectsTable.getColumnByName("id")
