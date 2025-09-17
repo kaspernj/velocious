29e05fa949891e065b59e1e8d90dd3327417e833
kaspernj <kasper@diestoeckels.de>
2025-09-17 06:47:41 +0000

    Fixed args

--- src/utils/with-tracked-stack.js
+++ src/utils/with-tracked-stack.js
@@ -5,6 +5,14 @@ function addTrackedStackToError(error) {
 async function withTrackedStack(...args) {
   const withTrackedStack = globalThis.withTrackedStack?.withTrackedStack
 
+  let callback
+
+  if (args[1]) {
+    callback = args[1]
+  } else {
+    callback = args[0]
+  }
+
   if (withTrackedStack) {
     return await withTrackedStack(...args)
   } else {
