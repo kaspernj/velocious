af773070c9160e3f9dca2dd32fdf2d90913c73b8
kaspernj <kasper@diestoeckels.de>
2025-09-21 16:46:26 +0000

    Found and fixed more issues

--- spec/database/record/create-spec.js
+++ spec/database/record/create-spec.js
@@ -2,12 +2,15 @@ import Dummy from "../../dummy/index.js"
 import Project from "../../dummy/src/models/project.js"
 import Task from "../../dummy/src/models/task.js"
 import {ValidationError} from "../../../src/database/record/index.js"
+import ProjectDetail from "../../dummy/src/models/project-detail.js"
 
 describe("Record - create", () => {
   it("creates a new simple record with relationships and translations", async () => {
     await Dummy.run(async () => {
       const task = new Task({name: "Test task"})
       const project = task.buildProject({nameEn: "Test project", nameDe: "Test projekt"})
+
+      project.buildProjectDetail({note: "Test note"})
 
       await task.save()
 
@@ -25,6 +28,12 @@ describe("Record - create", () => {
 
       // 'name' is not a column but rather a column on the translation data model.
       expect(() => project.readColumn("name")).toThrowError("No such attribute or not selected Project#name")
+
+      // It saves a project note
+      const projectDetail = project.projectDetail()
+
+      expect(projectDetail.note()).toEqual("Test note")
+      expect(projectDetail.projectId()).toEqual(project.id())
     })
   })
 
@@ -65,15 +74,12 @@ describe("Record - create", () => {
       }
 
       const projectsCount = await Project.count()
+      const projectDetailsCount = await ProjectDetail.count()
       const tasksCount = await Task.count()
 
       expect(projectsCount).toEqual(0)
+      expect(projectDetailsCount).toEqual(0)
       expect(tasksCount).toEqual(0)
-
-      const projectNote = project.projectNote()
-
-      expect(projectNote.note()).toEqual("Test note")
-      expect(projectNote.projectId()).toEqual(project.id())
     })
   })
 })
