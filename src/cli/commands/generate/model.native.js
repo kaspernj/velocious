9b4afc0b870804cef6ac339d31b9a4f87fcf4b3a
kaspernj <kasper@diestoeckels.de>
2025-12-02 16:35:19 +0000

    Native command files to avoid invalid imports

---
+++ src/cli/commands/generate/model.native.js
@@ -0,0 +1,7 @@
+import BaseCommand from "../../base-command.js"
+
+export default class DbGenerateModel extends BaseCommand {
+  async execute() {
+    throw new Error("Unsupported on native")
+  }
+}
