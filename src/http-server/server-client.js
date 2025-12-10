42edb85d768f3105f25a32f218a1d36632519b29
kaspernj <kasper@diestoeckels.de>
2025-12-10 15:03:21 +0000

    ServerClient type safety

--- src/http-server/server-client.js
+++ src/http-server/server-client.js
@@ -1,9 +1,17 @@
+// @ts-check
+
 import EventEmitter from "events"
 import {Logger} from "../logger.js"
 
 export default class ServerClient {
   events = new EventEmitter()
 
+  /**
+   * @param {object} args
+   * @param {import("../configuration.js").default} args.configuration
+   * @param {import("net").Socket} args.socket
+   * @param {number} args.clientCount
+   */
   constructor({configuration, socket, clientCount}) {
     if (!configuration) throw new Error("No configuration given")
 
@@ -19,11 +27,15 @@ export default class ServerClient {
 
   end() {
     return new Promise((resolve) => {
-      this.socket.once("close", () => resolve())
+      this.socket.once("close", () => resolve(null))
       this.socket.end()
     })
   }
 
+  /**
+   * @param {Buffer} chunk
+   * @returns {void}
+   */
   onSocketData = (chunk) => {
     this.logger.debug(() => [`Socket ${this.clientCount}: ${chunk}`])
 
@@ -34,11 +46,18 @@ export default class ServerClient {
     })
   }
 
+  /**
+   * @returns {void}
+   */
   onSocketEnd = () => {
     this.logger.debug(`Socket ${this.clientCount} end`)
     this.events.emit("close", this)
   }
 
+  /**
+   * @param {string} data
+   * @returns {Promise<void>}
+   */
   async send(data) {
     return new Promise((resolve) => {
       this.logger.debug("Send", data)
