cb5b43688aebb5fe955c3323b8c5a118cfdae338
kaspernj <kasper@diestoeckels.de>
2025-12-15 13:09:01 +0000

    Fixed namespace routes and added missing specs

--- spec/dummy/src/config/routes.js
+++ spec/dummy/src/config/routes.js
@@ -3,6 +3,10 @@ import Routes from "../../../../src/routes/index.js"
 const routes = new Routes()
 
 routes.draw((route) => {
+  route.namespace("api", (route) => {
+    route.post("version")
+  })
+
   route.resources("projects")
 
   route.resources("tasks", (route) => {
