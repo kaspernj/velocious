292696d3a91132981c5b81f4cfdb223e013dc970
kaspernj <kasper@diestoeckels.de>
2025-12-23 16:16:39 +0000

    Worked more on issues

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd spec/dummy && VELOCIOUS_TEST_DIR=/home/dev/Development/velocious/spec npx velocious test
-
+cd spec/dummy && VELOCIOUS_TEST_DIR=/home/dev/Development/velocious/spec node ../../bin/velocious.js test
--- src/http-server/client/params-to-object.js
+++ src/http-server/client/params-to-object.js
@@ -1,10 +1,5 @@
 // @ts-check
 
-/**
- * @typedef {Record<string, any>} ParamsObject
- * @typedef {ParamsObject | any[]} ParamsBranch
- */
-
 export default class ParamsToObject {
   /**
    * @param {Record<string, string>} object
@@ -13,9 +8,8 @@ export default class ParamsToObject {
     this.object = object
   }
 
-  /** @returns {ParamsObject} */
+  /** @returns {Record<string, any>} */
   toObject() {
-    /** @type {ParamsObject} */
     const result = {}
 
     for(const key in this.object) {
@@ -30,7 +24,7 @@ export default class ParamsToObject {
   /**
    * @param {string} key
    * @param {string} value
-   * @param {ParamsObject} result
+   * @param {Record<string, any> | any[]} result
    * @returns {void}
    */
   treatInitial(key, value, result) {
@@ -39,7 +33,19 @@ export default class ParamsToObject {
     if (firstMatch) {
       const inputName = firstMatch[1]
       const rest = firstMatch[2]
-      const newResult = this._getOrCreateBranch(result, inputName, rest)
+
+      /** @type {Array<any> | Record<string, any>} */
+      let newResult
+
+      if (inputName in result) {
+        newResult = result[inputName]
+      } else if (rest == "[]") {
+        newResult = []
+        result[inputName] = newResult
+      } else {
+        newResult = {}
+        result[inputName] = newResult
+      }
 
       this.treatSecond(value, rest, newResult)
     } else {
@@ -50,7 +56,7 @@ export default class ParamsToObject {
   /**
    * @param {string} value
    * @param {string} rest
-   * @param {ParamsBranch} result
+   * @param {Record<string, any> | any[]} result
    * @returns {void}
    */
   treatSecond(value, rest, result) {
@@ -61,70 +67,25 @@ export default class ParamsToObject {
     const key = secondMatch[1]
     const newRest = secondMatch[2]
 
+    /** @type {Array<any> | Record<string, any>} */
+    let newResult
+
     if (rest == "[]") {
-      this._ensureArray(result).push(value)
-      return
-    }
-
-    if (newRest == "") {
-      if (Array.isArray(result)) {
-        result[this._coerceArrayIndex(key)] = value
+      result.push(value)
+    } else if (newRest == "") {
+      result[key] = value
+    } else {
+      if (typeof result == "object" && key in result) {
+        newResult = result[key]
+      } else if (newRest == "[]") {
+        newResult = []
+        result[key] = newResult
       } else {
-        result[key] = value
-      }
-      return
-    }
-
-    const newResult = this._getOrCreateBranch(result, key, newRest)
-
-    this.treatSecond(value, newRest, newResult)
-  }
-
-  /**
-   * @param {ParamsBranch} branch
-   * @param {string} key
-   * @param {string} rest
-   * @returns {ParamsBranch}
-   */
-  _getOrCreateBranch(branch, key, rest) {
-    if (Array.isArray(branch)) {
-      if (key in branch && typeof branch[key] == "object") {
-        return /** @type {ParamsBranch} */ (branch[key])
+        newResult = {}
+        result[key] = newResult
       }
 
-      const newBranch = rest == "[]" ? [] : {}
-      branch[key] = newBranch
-
-      return newBranch
+      this.treatSecond(value, newRest, newResult)
     }
-
-    if (key in branch && typeof branch[key] == "object") {
-      return /** @type {ParamsBranch} */ (branch[key])
-    }
-
-    const newBranch = rest == "[]" ? [] : {}
-    branch[key] = newBranch
-
-    return newBranch
-  }
-
-  /**
-   * @param {ParamsBranch} result
-   * @returns {any[]}
-   */
-  _ensureArray(result) {
-    if (!Array.isArray(result)) throw new Error("Expected array when pushing to params result")
-
-    return result
-  }
-
-  /**
-   * @param {string} key
-   * @returns {number | string}
-   */
-  _coerceArrayIndex(key) {
-    const index = Number(key)
-
-    return Number.isNaN(index) ? key : index
   }
 }
