5367375d85bc195a2215cc439a5b257e6e55c9db
kaspernj <kasper@diestoeckels.de>
2025-12-26 15:59:14 +0000

    Fix distinct count for grouped queries

--- src/database/query/model-class-query.js
+++ src/database/query/model-class-query.js
@@ -72,6 +72,7 @@ export default class VelociousDatabaseQueryModelClassQuery extends DatabaseQuery
     // Clone query and execute count
     const countQuery = this.clone()
 
+    countQuery._distinct = false
     countQuery._selects = []
     countQuery.select(sql)
 
