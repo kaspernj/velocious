33314674cc92bea1fbc93d66c0bdc93dc85b1c7a
kaspernj <kasper@diestoeckels.de>
2025-12-28 20:28:18 +0000

    Fixed spec

--- spec/dummy/src/database/migrations/20251228090010-add-is-active-to-project-details.js
+++ spec/dummy/src/database/migrations/20251228090010-add-is-active-to-project-details.js
@@ -4,4 +4,8 @@ export default class AddIsActiveToProjectDetails extends Migration {
   async change() {
     await this.addColumn("project_details", "is_active", "boolean")
   }
+
+  async down() {
+    await this.removeColumn("project_details", "is_active")
+  }
 }
