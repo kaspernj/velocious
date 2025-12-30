c850d19cb31e533b482f44c42ecd153a16b28cf1
kaspernj <kasper@diestoeckels.de>
2025-12-30 18:16:21 +0000

    Set test environment for test command

--- src/cli/commands/test.js
+++ src/cli/commands/test.js
@@ -2,6 +2,8 @@ import BaseCommand from "../base-command.js"
 
 export default class VelociousCliCommandsTest extends BaseCommand {
   async execute() {
+    this.getConfiguration().setEnvironment("test")
+
     return await this.getConfiguration().getEnvironmentHandler().cliCommandsTest(this)
   }
 }
