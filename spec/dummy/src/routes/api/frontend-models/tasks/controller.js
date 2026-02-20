9f1f8fda6038cc673bd69dc083e0b1cc108de244
kaspernj <kasper@diestoeckels.de>
2026-02-20 16:19:56 +0000

    Add dummy-app frontend model authorization e2e coverage

---
+++ spec/dummy/src/routes/api/frontend-models/tasks/controller.js
@@ -0,0 +1,4 @@
+import FrontendModelController from "../../../../../../../src/frontend-model-controller.js"
+
+/** Dummy frontend-model controller for Task resource command endpoints. */
+export default class ApiFrontendModelsTasksController extends FrontendModelController {}
