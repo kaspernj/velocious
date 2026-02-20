3133bd0351e3c5b2eba1acc6045542768c43f5fa
kaspernj <kasper@diestoeckels.de>
2026-02-20 17:37:23 +0000

    Add explicit dummy routes for frontend-model task commands

--- spec/dummy/src/routes/api/frontend-models/tasks/controller.js
+++ spec/dummy/src/routes/api/frontend-models/tasks/controller.js
@@ -1,4 +1,24 @@
 import FrontendModelController from "../../../../../../../src/frontend-model-controller.js"
 
 /** Dummy frontend-model controller for Task resource command endpoints. */
-export default class ApiFrontendModelsTasksController extends FrontendModelController {}
+export default class ApiFrontendModelsTasksController extends FrontendModelController {
+  /** @returns {Promise<void>} */
+  async list() {
+    await this.frontendIndex()
+  }
+
+  /** @returns {Promise<void>} */
+  async find() {
+    await this.frontendFind()
+  }
+
+  /** @returns {Promise<void>} */
+  async update() {
+    await this.frontendUpdate()
+  }
+
+  /** @returns {Promise<void>} */
+  async destroy() {
+    await this.frontendDestroy()
+  }
+}
