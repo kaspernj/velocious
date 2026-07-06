c13196947e5ea50c2e6321298ee54eb45c13f950
kaspernj <kasper@diestoeckels.de>
2026-07-06 21:15:00 +0000

    Remove explicit type from project_audits FK references

--- spec/dummy/src/database/migrations/20260706100000-create-project-audits.js
+++ spec/dummy/src/database/migrations/20260706100000-create-project-audits.js
@@ -3,8 +3,8 @@ import Migration from "../../../../../src/database/migration/index.js"
 export default class CreateProjectAudits extends Migration {
   async up() {
     await this.createTable("project_audits", (table) => {
-      table.references("project", {foreignKey: true, null: false, type: "bigint"})
-      table.references("audit_action", {foreignKey: true, null: false, type: "bigint"})
+      table.references("project", {foreignKey: true, null: false})
+      table.references("audit_action", {foreignKey: true, null: false})
       table.json("audited_changes")
       table.json("params")
       table.timestamps()
